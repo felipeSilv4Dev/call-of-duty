656aa4b7b48cd3b2230e588aa81928bb45c3affe
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-12-05 16:44:42 -0300

    adding menu mobile

--- js/menuMobile/menuMobile.js
+++ js/menuMobile/menuMobile.js
@@ -1,5 +1,22 @@
 const menuMobile = () => {
-  console.log("oi");
+  const buttonEl = document.querySelector(".mobile__button");
+  const overlayEl = document.querySelector(".overlay");
+
+  const handleClick = ({ target }) => {
+    if (
+      target.classList.contains("mobile__button") ||
+      target.classList.contains("overlay")
+    ) {
+      buttonEl.classList.toggle("active");
+      overlayEl.classList.toggle("active");
+      buttonEl.classList.contains("active")
+        ? (document.body.style.overflow = "hidden")
+        : (document.body.style.overflow = "initial");
+    }
+  };
+
+  buttonEl.addEventListener("click", handleClick);
+  overlayEl.addEventListener("click", handleClick);
 };
 
 export default menuMobile;
