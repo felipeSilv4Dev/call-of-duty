9d9a7cb12129ff86d1b471d9e7443c5f8f1eb8ae
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-12-06 20:28:03 -0300

    Ajusting responsive header added

--- js/menuMobile/menuMobile.js
+++ js/menuMobile/menuMobile.js
@@ -1,17 +1,21 @@
 const menuMobile = () => {
   const buttonEl = document.querySelector(".mobile__button");
   const overlayEl = document.querySelector(".overlay");
+  const body = document.querySelector("body");
+  let observe = false;
 
   const handleClick = ({ target }) => {
+    observe = !observe;
+
     if (
       target.classList.contains("mobile__button") ||
       target.classList.contains("overlay")
     ) {
+      observe
+        ? (body.style.overflow = "hidden")
+        : (body.style.overflow = "initial");
       buttonEl.classList.toggle("active");
       overlayEl.classList.toggle("active");
-      buttonEl.classList.contains("active")
-        ? (document.body.style.overflow = "hidden")
-        : (document.body.style.overflow = "initial");
     }
   };
 
